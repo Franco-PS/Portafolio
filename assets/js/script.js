af3fd8685ac3a2ef36ad9e79c13e430c57b62946
Franco <perez_salazarf@hotmail.com>
2021-08-18 13:53:18 -0500

    Mejorar apariencia,Banner-skill-contac

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,14 @@ $(document).ready(()=>{
 
 
 
-const button1= document.getElementById('skill__click1')
-const button2= document.getElementById('skill__click2')
-const block1= document.getElementById('skill-show1')
-const block2= document.getElementById('skill-show2')
+// const button1= document.getElementById('skill__click1')
+// const button2= document.getElementById('skill__click2')
+// const block1= document.getElementById('skill-show1')
+// const block2= document.getElementById('skill-show2')
 
-button1.addEventListener('click',()=>{
-    block1.classList.toggle('show')
-})
-button2.addEventListener('click',()=>{
-    block2.classList.toggle('show')
-})
+// button1.addEventListener('click',()=>{
+//     block1.classList.toggle('show')
+// })
+// button2.addEventListener('click',()=>{
+//     block2.classList.toggle('show')
+// })
