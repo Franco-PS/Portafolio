88907390e15295f421de45281a43a96a181ec719
Franco <perez_salazarf@hotmail.com>
2021-09-02 16:20:31 -0500

    cambio mail.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
     $destino="perez_salazarf@hotmail.com";
-    $nombre= "$_Post["nombre"]";
-    $correo= "$_Post["correo"]";
-    $asunto= "$_Post["asunto"]";
-    $mensaje= "$_Post["mensaje"]";
+    $nombre= "$_POST["nombre"]";
+    $correo= "$_POST["correo"]";
+    $asunto= "$_POST["asunto"]";
+    $mensaje= "$_POST["mensaje"]";
     $contenido= "nombre".$nombre . "\ncorreo". $correo . "\nasunto" . $asunto . "\nmensaje" .  $mensaje;
     mail($destino,"asunto",$contenido);
     header("Location:index.html");
