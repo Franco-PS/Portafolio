2798cd273bcd79b5b859e3e1f4b84353c0aad9cc
franko <perez_salazarf@hotmail.com>
2022-02-24 02:16:05 -0600

    mejora banner

--- assets/js/banner-cv.js
+++ assets/js/banner-cv.js
@@ -1,26 +1,3 @@
-// const nuevoDoc= document.getElementById('downloadBtn');
-// nuevoDoc.addEventListener('click', e=>{
-    
-//     let windowShow= document.createElement('div');
-//     let areaShow= document.getElementById('main');
-    
-//     windowShow.id="showPdf"
-//     windowShow.classList.add('block__window')
-//     windowShow.innerHTML=`
-//     <div class="block__pdf" ></div>
-//     `
-   
-//     areaShow.append(windowShow)
-//     if(windowShow){
-//         windowShow.addEventListener('click',e=>{
-//             console.log(e.target)
-//             let ver= document.getElementById('showPdf')
-//             ver.remove(windowShow)
-//         })
-//     }
-    
-
-// })
 const openElem= document.getElementById('btnCV');
 const openmodal= document.getElementById('body');
 
@@ -30,7 +7,7 @@ openElem.addEventListener('click',()=>{
     elemWindow.classList.add('block__window');
     elemWindow.innerHTML=`
     <div class="block__pdf container" >
-    <iframe src="./assets/img/Francisco-cv.pdf" width="100%" height="100%">
+    <iframe src="./assets/img/franciscocv.pdf" width="100%" height="100%">
     </div>
     `
     openmodal.append(elemWindow);
